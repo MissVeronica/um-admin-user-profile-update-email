62f14bc5e9b335ac0f0905f88dc5bc327d9e6603
MissVeronica <veronica@tranza.se>
2023-07-22 20:42:10 +0200

    Update um-admin-user-profile-update-email.php

--- um-admin-user-profile-update-email.php
+++ um-admin-user-profile-update-email.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:     Ultimate Member - Admin Email Profile Update
  * Description:     Extension to Ultimate Member with an email template for sending an email to the site admin when an UM User Profile is updated.
- * Version:         3.0.0
+ * Version:         3.1.0
  * Requires PHP:    7.4
  * Author:          Miss Veronica
  * License:         GPL v2 or later
@@ -19,12 +19,17 @@ if ( ! class_exists( 'UM' ) ) return;
 
 Class UM_Admin_Email_Profile_Update {
 
+    public $slug = array();
+
     function __construct() {
 
         add_filter( 'um_email_notifications',                 array( $this, 'custom_email_notifications_profile_is_updated' ), 10, 1 );
         add_action( 'um_user_after_updating_profile',         array( $this, 'custom_profile_is_updated_email' ), 10, 3 );
         add_action( 'profile_update',                         array( $this, 'custom_profile_is_updated_email_backend' ), 10, 3 );
         add_filter( 'um_admin_settings_email_section_fields', array( $this, 'um_admin_settings_email_section_fields_custom_forms' ), 10, 2 );
+        add_action(	'um_extend_admin_menu',                   array( $this, 'copy_email_notifications_admin_profile_update' ), 10 );
+
+        define( 'Admin_Email_Profile_Update_Path', plugin_dir_path( __FILE__ ) );
     }
 
     public function custom_email_notifications_profile_is_updated( $emails ) {
@@ -43,6 +48,17 @@ Class UM_Admin_Email_Profile_Update {
                                                         'profile_is_updated_email_sub' => 'Profile Update {username}', ), 
                                                 UM()->options()->options );
 
+        foreach ( $custom_emails as $slug => $custom_email ) {
+
+            if ( ! array_key_exists( $slug . '_on', UM()->options()->options ) ) {
+
+                UM()->options()->options[ $slug . '_on' ]  = empty( $custom_email['default_active'] ) ? 0 : 1;
+                UM()->options()->options[ $slug . '_sub' ] = $custom_email['subject'];
+            }
+
+            $this->slug[] = $slug;
+        }
+
         return array_merge( $custom_emails, $emails );
     }
 
@@ -60,6 +76,27 @@ Class UM_Admin_Email_Profile_Update {
         }
 
         return $section_fields;
+    }
+
+    public function copy_email_notifications_admin_profile_update() {
+
+        foreach( $this->slug as $slug ) {
+
+            $located = UM()->mail()->locate_template( $slug );
+
+            if ( ! is_file( $located )) {
+                $located = wp_normalize_path( get_stylesheet_directory() . '/ultimate-member/email/' . $slug . '.php' );
+            }
+
+            clearstatcache();
+            if ( ! file_exists( $located ) || filesize( $located ) == 0 ) {
+
+                wp_mkdir_p( dirname( $located ) );
+
+                $email_source = file_get_contents( Admin_Email_Profile_Update_Path . $slug . '.php' );
+                file_put_contents( $located, $email_source );
+            }
+        }
     }
 
     public function custom_profile_is_updated_email_backend( $user_id, $old_data, $user_data ) {
@@ -119,4 +156,3 @@ Class UM_Admin_Email_Profile_Update {
 }
 
 new UM_Admin_Email_Profile_Update();
-
